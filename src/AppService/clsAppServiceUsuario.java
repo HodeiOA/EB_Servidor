ac840c41f35a56b13b52e24b36a8e22b9f9f3c07
unknown <maiderc@opendeusto.es>
2019-05-03 09:22:50 +0200

    AppService pattern started

---
+++ src/AppService/clsAppServiceUsuario.java
@@ -0,0 +1,52 @@
+package AppService;
+
+import ObjetosDominio.clsUsuario;
+import APIs.itfSistAutorizacion;
+
+public class clsAppServiceUsuario 
+{
+	boolean RegistrarUsuario (clsUsuario nuevoUsuario)
+	{
+		boolean retorno = false;
+		//Antes de esto, en swing, darle valor a SistemaAutorización de usuario (Google o facebook)
+		itfSistAutorizacion i = nuevoUsuario.getSistemaAutorizacion(); 
+		//Vemos si el usuario es reconocido por la API externa
+		boolean b = i.ValidarUsuario(nuevoUsuario.getEmail());
+		
+		if(b)
+		{
+			//El usuario ya estaba en la BD, no se puede rregistrar
+			retorno = false;
+		}
+		else
+		{
+			retorno = true;
+			//TO-DO: Llamada a DAO Para gaurdar
+			//DAO.guardarObjeto(nuevoUsuario);
+		}
+		return false;		
+	}
+	
+	clsUsuario LoginUsuario (clsUsuario nuevoUsuario)
+	{
+		clsUsuario u = new clsUsuario();
+		boolean retorno = false;
+		//Antes de esto, en swing, darle valor a SistemaAutorización de usuario (Google o facebook)
+		itfSistAutorizacion i = nuevoUsuario.getSistemaAutorizacion(); 
+		//Vemos si el usuario es reconocido por la API externa
+		boolean b = i.ValidarUsuario(nuevoUsuario.getEmail());
+		
+		if(b)
+		{
+			//El usuario ya estaba en la BD, puede loggearse
+			retorno = true;
+			//Leemos de la BD el usuario con ese email y lo devolvemos con todos sus atributos
+			//DAO
+		}
+		else
+		{
+			retorno = false;
+		}
+		return u;	
+	}
+}
---
+++ src/AppService/clsAppServiceVuelo.java
@@ -0,0 +1,98 @@
+package AppService;
+
+import java.sql.Date;
+import java.util.ArrayList;
+
+import APIs.itfPasarela;
+import ObjetosDominio.clsAeropuerto;
+import ObjetosDominio.clsReserva;
+import ObjetosDominio.clsUsuario;
+import ObjetosDominio.clsVuelo;
+
+public class clsAppServiceVuelo 
+{
+	ArrayList <clsVuelo> BuscarVueloIda (clsAeropuerto aeropuesrtoPred, Date fecha, String ciudadOrigen, String ciudadDestino)
+	{
+		//Lee todos los vuelos de DAO
+		ArrayList <clsVuelo> TodosVuelos = new ArrayList<clsVuelo>(); //Leer de DAO con el método de itfCargaVuelos:cargarIda
+		ArrayList <clsVuelo> VuelosRetorno  = new ArrayList<clsVuelo>();
+		
+		//En la primera vuelta sólo metemos los que tengan destino u origen igual al prefdeterminado
+		for (clsVuelo v: TodosVuelos)
+		{	
+			if( (v.getAeropuertoOrigen()).equals(aeropuesrtoPred) || (v.getAeropuertoDestino()).equals(aeropuesrtoPred))
+				VuelosRetorno.add(v);
+		}
+		
+		//Una vez quedan lo más arriba los del aeropuesrto predeterminado, metemos el resto
+		for (clsVuelo v: TodosVuelos)
+		{	
+			if( !((v.getAeropuertoOrigen()).equals(aeropuesrtoPred)) || !((v.getAeropuertoDestino()).equals(aeropuesrtoPred)))
+				VuelosRetorno.add(v);
+		}
+		return VuelosRetorno;
+	}
+	
+	ArrayList <clsVuelo> BuscarVueloIdayVuelta (clsAeropuerto aeropuesrtoPred, Date fechaIda, Date fechaVuelta, String ciudadOrigen, String ciudadDestino)
+	{
+		
+		//Lee todos los vuelos de DAO
+		ArrayList <clsVuelo> TodosVuelos = new ArrayList<clsVuelo>(); //Leer de DAO con el método de itfCargaVuelos: cargarIdaVuelta
+		ArrayList <clsVuelo> VuelosRetorno  = new ArrayList<clsVuelo>();
+		
+		//En la primera vuelta sólo metemos los que tengan destino u origen igual al prefdeterminado
+		for (clsVuelo v: TodosVuelos)
+		{	
+			if( (v.getAeropuertoOrigen()).equals(aeropuesrtoPred) || (v.getAeropuertoDestino()).equals(aeropuesrtoPred))
+				VuelosRetorno.add(v);
+		}
+		
+		//Una vez quedan lo más arriba los del aeropuesrto predeterminado, metemos el resto
+		for (clsVuelo v: TodosVuelos)
+		{	
+			if( !((v.getAeropuertoOrigen()).equals(aeropuesrtoPred)) || !((v.getAeropuertoDestino()).equals(aeropuesrtoPred)))
+				VuelosRetorno.add(v);
+		}
+		return VuelosRetorno;
+		
+	}
+
+	ArrayList <clsVuelo> BuscarVueloCualquierMomento (clsAeropuerto aeropuesrtoPred, String ciudadOrigen, String ciudadDestino)
+	{
+		
+		//Lee todos los vuelos de DAO
+		ArrayList <clsVuelo> TodosVuelos = new ArrayList<clsVuelo>(); //Leer de DAO con el método de itfCargaVuelos: cargarCualquierMomento
+		ArrayList <clsVuelo> VuelosRetorno  = new ArrayList<clsVuelo>();
+		
+		//En la primera vuelta sólo metemos los que tengan destino u origen igual al prefdeterminado
+		for (clsVuelo v: TodosVuelos)
+		{	
+			if( (v.getAeropuertoOrigen()).equals(aeropuesrtoPred) || (v.getAeropuertoDestino()).equals(aeropuesrtoPred))
+				VuelosRetorno.add(v);
+		}
+		
+		//Una vez quedan lo más arriba los del aeropuesrto predeterminado, metemos el resto
+		for (clsVuelo v: TodosVuelos)
+		{	
+			if( !((v.getAeropuertoOrigen()).equals(aeropuesrtoPred)) || !((v.getAeropuertoDestino()).equals(aeropuesrtoPred)))
+				VuelosRetorno.add(v);
+		}
+		return VuelosRetorno;
+	}
+
+	boolean RealizarPagoyReserva (clsUsuario usuario, clsVuelo vuelo, int numAsiento, String nomViajero)
+	{
+		boolean pago = false;
+		clsReserva reserva = new clsReserva(numAsiento, vuelo, usuario, nomViajero, vuelo.getPrecio());
+		itfPasarela p = usuario.getPasarelaDePago();
+		//pago = p.ValidarPago
+		
+		if(pago)
+		{
+			//TO DO: Escribimos la reserva con dao
+		}
+		vuelo.setNumAsientosLibres(vuelo.getNumAsientosLibres() - 1);
+		//TO DO: Escribimos el vuelo con dao
+		return pago;		
+	}
+}
